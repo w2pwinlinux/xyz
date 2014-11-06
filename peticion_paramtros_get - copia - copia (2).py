ffd34c5b2c85729b97190b6129a55b334873a361
w2p <wilsonrobertocunalata@gmail.com>
2014-11-05 21:19:55 -0500

    borrado2

--- peticion_paramtros_get - copia - copia (2).py
+++
@@ -1,6 +0,0 @@
-import httplib
-connection = httplib.HTTPConnection("www.rubenbernardez.com")
-connection.request("GET","/blog/")
-response = connection.getresponse()
-data = response.read()
-connection.close()
